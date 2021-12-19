522bc27da289483b14bb12ce7f019cd365ffdac2
Cahllagerfeld <Cahllagerfeld>
2021-12-19 10:55:09 +0000

    feat: update initial setup

---
+++ src/lib/Button/Button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let cta: boolean;
+	export let label: string;
+</script>
+
+<button class="button" class:cta>{label}</button>
+
+<style lang="text/postcss">
+	@apply text-2xl;
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'brand-ripe': '#ff8a00',
+				'brand-almost-ripe': '#ffae33',
+				'sand-dark': '#ece7e5',
+				'sand-light': '#f5f4f4'
+			}
+		}
 	},
 
 	plugins: []
