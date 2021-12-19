068ccee164706858fb9f147a626d3fff46208aa4
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2021-12-19 12:41:34 +0100

    feat: update initial setup (#1)
    * feat: update initial setup
    
    * feat: add cta button

---
+++ src/lib/Button/Button.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let cta: boolean = false;
+	export let label: string;
+</script>
+
+<button class="button" class:cta {...$$restProps}>{label}</button>
+
+<style lang="postcss">
+	.button {
+		@apply inline-block text-center font-semibold rounded-xl leading-4 bg-sand-dark px-6 py-2 shadow-light;
+	}
+
+	.cta {
+		@apply hover:bg-brand-almost-ripe;
+	}
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'brand-ripe': '#ff8a00',
+				'brand-almost-ripe': '#ffae33',
+				'sand-dark': '#ece7e5',
+				'sand-light': '#f5f4f4'
+			},
+			boxShadow:{
+				light: "0 1px 1px rgba(0, 0, 0, 0.1)"
+			}
+		}
 	},
 
 	plugins: []
