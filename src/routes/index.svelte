a42beaa2461504cc211c2ffe539b0d240bdf945b
Cahllagerfeld <Cahllagerfeld>
2021-12-19 19:37:58 +0000

    feat: add type to component

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,8 @@
 <script>
-	import {Button} from 'cahllagercomponents';
+	import { Button } from 'cahllagercomponents';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<Button cta label="Hallo Welt" />
+<Button color="cta" label="Hallo Welt" />
