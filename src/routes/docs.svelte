552853bd49d03d04afc3dee59f14cf1aea536a6e
Cahllagerfeld <Cahllagerfeld>
2021-12-20 16:53:38 +0000

    feat: add 3 col layout

---
+++ src/routes/docs.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { loremIpsum } from '$lib/consts/loremipsum';
+</script>
+
+<div class="sticky top-0 h-24 bg-indigo-50" />
+<div class="bg-indigo-100  lg:fixed lg:left-0 lg:h-full lg:w-1/5">Left</div>
+<div class="bg-indigo-300 lg:fixed lg:h-full lg:right-0 lg:w-1/5">Right</div>
+<div class="bg-indigo-200 lg:overflow-scroll lg:w-3/5" style="margin: 0 auto;">{loremIpsum}</div>
