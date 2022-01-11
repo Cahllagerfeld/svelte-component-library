67e54dee59a93da2a16c046bc2262fa6a4c37c10
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-11 10:00:27 +0000

    feat: add button types

--- src/lib/Button/button.svelte
+++ src/lib/Button/button.svelte
@@ -3,6 +3,9 @@
 
 	export let variant: ButtonVariant;
 	export let disabled: true | false = false;
+
+	let clazz = '';
+	export { clazz as class };
 </script>
 
 <button
@@ -19,7 +22,7 @@
 	on:keypress
 	on:keydown
 	on:keyup
-	class="inline-block text-center font-semibold rounded-xl leading-4 bg-sand-dark px-6 py-2 shadow-light border-none cursor-pointer {variant}"
+	class="inline-block text-center font-semibold rounded-xl leading-4 bg-sand-dark px-6 py-2 shadow-light border-none cursor-pointer {variant} {clazz}"
 	class:disabled
 	{disabled}><slot /></button
 >
@@ -27,5 +30,17 @@
 <style lang="postcss">
 	.primary {
 		@apply bg-kumquat-ripe hover:bg-kumquat-almost-ripe;
+	}
+
+	.secondary {
+		@apply bg-salmon hover:bg-salmon-hover;
+	}
+
+	.cta {
+		@apply bg-sand-dark hover:bg-kumquat-almost-ripe;
+	}
+
+	.tertiary {
+		@apply bg-black text-white hover:bg-black-hover;
 	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,6 +6,9 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <div class="flex justify-center ">
 	<Card variant="extralarge">
-		<Button variant="primary">Text</Button>
+		<Button class="mb-2" variant="primary">Text</Button>
+		<Button class="mb-2" variant="secondary">Text</Button>
+		<Button class="mb-2" variant="cta">Text</Button>
+		<Button class="mb-2" variant="tertiary">Text</Button>
 	</Card>
 </div>
