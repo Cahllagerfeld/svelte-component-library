42f9c8c9114247067c44dbd45f6e838b75e191a0
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-15 22:05:21 +0000

    feat: add gsap action

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,11 @@
 <script>
 	import { Card, Button } from 'cahllagercomponents';
+	import { animate } from '$lib/gsap';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<div class="flex justify-center ">
+<div use:animate={{ type: 'from', duration: 3, opacity: 0 }} class="flex justify-center ">
 	<Card variant="extralarge">
 		<Button class="mb-2" variant="primary">Text</Button>
 		<Button class="mb-2" variant="secondary">Text</Button>
