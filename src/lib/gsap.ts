83376331b57f389828b3bc14ff616c7f0f121b5b
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-01-16 10:22:50 +0000

    feat: add interface

--- src/lib/gsap.ts
+++ src/lib/gsap.ts
@@ -1,6 +1,10 @@
 import { gsap } from 'gsap';
 
-export function animate(node: Element, { type, ...args }) {
+export function animate(node: gsap.TweenTarget, { type, ...args }: animateProps) {
 	const method = gsap[type];
 	return method(node, args);
+}
+
+export interface animateProps extends gsap.TweenVars {
+	type: string;
 }
