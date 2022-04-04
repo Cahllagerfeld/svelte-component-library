fa025bc461f04de16402257dbd14d1ca702cb246
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-04-04 20:02:21 +0000

    feat: use map

--- src/lib/code/open-vsx-extension.svelte
+++ src/lib/code/open-vsx-extension.svelte
@@ -2,9 +2,11 @@
 	import type { OpenVsxExtension } from './code';
 
 	export let extension: OpenVsxExtension.Extension;
+	export let checked: boolean = false;
 </script>
 
 <div
+	class:checked
 	class="bg-[#f9f9f9] shadow-md rounded-2xl p-4 space-y-4 cursor-pointer flex flex-col justify-between items-center"
 >
 	<img
@@ -17,3 +19,9 @@
 		{extension.displayName}
 	</h3>
 </div>
+
+<style lang="postcss">
+	.checked {
+		@apply border-4 border-orange-400;
+	}
+</style>
--- src/lib/code/open-vsx.svelte
+++ src/lib/code/open-vsx.svelte
@@ -3,13 +3,14 @@
 	import { onMount } from 'svelte';
 	import OpenVsxExtension from './open-vsx-extension.svelte';
 	import type { OpenVsxExtension as VSXType } from './code';
-	// export let searchString: string = '';
 	let skip: number = 0;
 	let data: VSXType.Extension[] = [];
 	let newData: VSXType.Extension[] = [];
 	let component: HTMLElement;
 	let search: string = '';
 	let isLoadMore: boolean = false;
+
+	let selectedExtensions: Map<string, VSXType.Extension> = new Map();
 
 	onMount(async () => {
 		performSearch('');
@@ -40,6 +41,7 @@
 </script>
 
 <div class="grid  p-8 grid-cols-3 gap-4">
+	{JSON.stringify(Array.from(selectedExtensions.values()))}
 	<div class="flex flex-col gap-4">
 		<input
 			placeholder="search your extension"
@@ -48,8 +50,24 @@
 			on:input={changeHandler}
 		/>
 		<div bind:this={component} class="grid  grid-cols-1  sm:grid-cols-2 lg:grid-cols-3 gap-4">
-			{#each data as extension}
-				<OpenVsxExtension {extension} />
+			{#each data as extension (extension.url)}
+				<input
+					on:input={() => {
+						if (selectedExtensions.has(extension.url)) {
+							selectedExtensions.delete(extension.url);
+							selectedExtensions = selectedExtensions;
+						} else {
+							selectedExtensions = selectedExtensions.set(extension.url, extension);
+						}
+					}}
+					value={extension}
+					type="checkbox"
+					id={extension.name}
+					class="hidden"
+				/>
+				<label for={extension.name}>
+					<OpenVsxExtension {extension} checked={selectedExtensions.has(extension.url)} />
+				</label>
 			{/each}
 
 			<InfiniteScroll
