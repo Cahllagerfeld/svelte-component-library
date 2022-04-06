53944c9f550995296c66e21543e5430328a4e627
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-04-06 07:51:39 +0000

    feat: dynamic radio buttons

---
+++ src/lib/contents/radio-buttons.ts
@@ -0,0 +1,7 @@
+export const radioButtons = [
+	{ value: 'env', label: 'Env' },
+	{ value: 'before', label: 'Before' },
+	{ value: 'init', label: 'Init' },
+	{ value: 'command', label: 'Command' },
+	{ value: 'openMode', label: 'Open Mode' }
+];
--- src/lib/radio-group.svelte
+++ src/lib/radio-group.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { radioButtons } from '$lib/contents/radio-buttons';
 	export let index: number;
 	export let step = 'env';
 </script>
@@ -6,26 +7,12 @@
 <div
 	class="inline-flex overflow-hidden items-center justify-center bg-white space-x-2 rounded-2xl transition-all duration-200"
 >
-	<label class="cursor-pointer px-4 py-2" class:active={step == 'env'}>
-		<input type="radio" class="hidden" bind:group={step} value="env" name={`${index}`} />
-		Env
-	</label>
-	<label class="cursor-pointer px-4 py-2" class:active={step == 'before'}>
-		<input type="radio" class="hidden" bind:group={step} value="before" name={`${index}`} />
-		Before
-	</label>
-	<label class="cursor-pointer px-4 py-2" class:active={step == 'init'}>
-		<input type="radio" class="hidden" bind:group={step} value="init" name={`${index}`} />
-		Init
-	</label>
-	<label class="cursor-pointer px-4 py-2" class:active={step == 'command'}>
-		<input type="radio" class="hidden" bind:group={step} value="command" name={`${index}`} />
-		Command
-	</label>
-	<label class="cursor-pointer px-4 py-2" class:active={step == 'openMode'}>
-		<input type="radio" class="hidden" bind:group={step} value="openMode" name={`${index}`} />
-		Open Mode
-	</label>
+	{#each radioButtons as { label, value }}
+		<label class="cursor-pointer px-4 py-2" class:active={step == value}>
+			<input type="radio" class="hidden" bind:group={step} {value} name={`${index}`} />
+			{label}
+		</label>
+	{/each}
 </div>
 
 <style lang="postcss">
